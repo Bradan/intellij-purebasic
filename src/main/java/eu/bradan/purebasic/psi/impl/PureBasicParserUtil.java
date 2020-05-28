f293ddb61cb91875ce906981a922fb8daacd8269
Bradan <git@bradan.eu>
2020-05-28 19:10:56 +0200

    Better error tolerance, structure union bugfix

---
+++ src/main/java/eu/bradan/purebasic/psi/impl/PureBasicParserUtil.java
@@ -0,0 +1,10 @@
+package eu.bradan.purebasic.psi.impl;
+
+import com.intellij.lang.PsiBuilder;
+import com.intellij.lang.parser.GeneratedParserUtilBase;
+
+public class PureBasicParserUtil extends GeneratedParserUtilBase {
+    public static boolean parseAlwaysTrueStatement(PsiBuilder builder, int level) {
+        return true;
+    }
+}
